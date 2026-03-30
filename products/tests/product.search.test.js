6a3134b96e8959da84815c3431b879eaa771c1c1
ju4n <juanoliveiraf90@gmail.com>
2026-03-30 10:58:42 -0300

    refactor product.search.test.js

--- products/tests/product.search.test.js
+++ products/tests/product.search.test.js
@@ -1,31 +1,30 @@
-import test from 'node:test'
-import assert from 'node:assert'
+import { test, describe, beforeEach } from 'node:test'
+import assert from 'node:assert/strict'
 import Build from '../app.js'
 
-test('search mechanism', async t => {
-  t.plan(1)
-  const server = Build()
-
-  t.after(() => server.close())
-
-  const res = await server.inject({
-    method: 'GET',
-    url: '/search/i9',
+describe('product tests', () => {
+  let server
+  
+  beforeEach(t => {
+    server = Build()
+    t.after(() => server.close())
   })
-  t.assert.strictEqual(res.statusCode, 200)
-})
-
-test('product details', async t => {
-  t.plan(1)
-  const server = Build()
-
-  t.after(() => server.close())
-
-  const res = await server.inject({
-    method: 'GET',
-    url: '/product/2'
+  
+  test('search mechanism', async t => {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/search/i9',
+    })
+    t.assert.strictEqual(res.statusCode, 200)
   })
-
-  console.log(res.body)
-  t.assert.strictEqual(res.statusCode, 200)
+  
+  test('product details', async t => {
+    const res = await server.inject({
+      method: 'GET',
+      url: '/product/2'
+    })
+    
+    //console.log(res.body)
+    t.assert.strictEqual(res.statusCode, 200)
+  })
 })
