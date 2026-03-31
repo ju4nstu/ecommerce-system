06a6da034afdc53f91e2030472682718e55fde17
ju4n <juanoliveiraf90@gmail.com>
2026-03-31 12:16:52 -0300

    automated tests

--- products/tests/product.search.test.js
+++ products/tests/product.search.test.js
@@ -10,21 +10,61 @@ describe('product tests', () => {
     t.after(() => server.close())
   })
   
-  test('search mechanism', async t => {
+  test('search', async t => {
     const res = await server.inject({
       method: 'GET',
-      url: '/search/i9',
+      url: '/search/ryzen',
     })
+    //console.log(res.body)
     t.assert.strictEqual(res.statusCode, 200)
   })
   
-  test('product details', async t => {
+  test('product detail', async t => {
     const res = await server.inject({
       method: 'GET',
       url: '/product/2'
     })
-    
-    //console.log(res.body)
+  
     t.assert.strictEqual(res.statusCode, 200)
   })
+  
+  describe('req queries', () => {
+    test('page_number input bigger than its possible', async t => {
+      const res = await server.inject({
+        method: 'GET',
+        url: '/?page_number=55457'
+      })
+  
+      t.assert.strictEqual(res.statusCode, 404, 'should return 404: NOT FOUND')
+    })
+    describe('sort options', () => {
+      test('price', async t => {
+        const res = await server.inject({
+          method: 'GET',
+          url: '/?sort=price'
+        })
+    
+        t.assert.strictEqual(res.statusCode, 200, 'should return 200: OK')
+      })
+      
+      test('-price', async t => {
+        const res = await server.inject({
+          method: 'GET',
+          url: '/?sort=-price'
+        })
+    
+        t.assert.strictEqual(res.statusCode, 200, 'should return 200: OK')
+      })
+
+      test('nonexistent', async t => {
+        const res = await server.inject({
+          method: 'GET',
+          url: '/?sort=hello'
+        })
+    
+        t.assert.strictEqual(res.statusCode, 200, 'should return 200: OK')
+      })
+    })
+  })
 })
+
