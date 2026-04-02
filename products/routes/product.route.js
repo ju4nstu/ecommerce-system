3017fd9577a8d40c8d39ed3f82e7eccbdbb576c0
Juan Oliveira <juanoliveiraf90@gmail.com>
2026-04-02 12:45:44 -0300

    Add root route for Products API

--- products/routes/product.route.js
+++ products/routes/product.route.js
@@ -3,6 +3,7 @@ import { Product } from "../services/product.details.js"
 import { List } from "../services/product.list.js"
 
 export default async function productRoutes(server, opts) {
+  server.get('/', (req, rep) => { rep.send('This is the Products API') })
   server.get('/search/:input', Search)
   server.get('/:id', Product)
   server.get('/', List)
